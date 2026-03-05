10de0bdd30dccd4e85b16c945b5c60494b67a8ab
Your Name <you@example.com>
2026-03-05 17:21:49 +0530

    createBrowserRouter setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,32 @@
 
-import { Button } from "@/components/ui/button"
-import './App.css'
+import {  createBrowserRouter } from "react-router-dom"
 import Navbar from "./components/shared/Navbar"
+import { RouterProvider } from "react-router-dom"
+import Home from "./components/Home"
+import SignUp from "./components/auth/SignUp.jsx"
+import Login from "./components/auth/Login.jsx"
+
 
 function App() {
  
-
+  const appRouter=createBrowserRouter([
+    {
+      path:'/',
+      element:<Home/>
+    },
+    {
+      path:'/signup',
+      element:<SignUp/>
+    },
+    {
+      path:'login',
+      element:<Login/>
+    }
+  ])
   return (
     <>
-       <Navbar/>
+      <RouterProvider router={appRouter}/>
+
     </>
   )
 }
--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
+import { LogOut, User2 } from "lucide-react";
 
 const Navbar = () => {
+  const user = true;
+
   return (
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
@@ -13,30 +16,51 @@ const Navbar = () => {
             Job<span className="text-red-500">Portal</span>{" "}
           </h1>
         </div>
-        <div className="flex item-center gap-12">
+        <div className="flex items-center gap-12">
           <ul className="flex font-medium items-center gap-5">
             <li>Home</li>
             <li>Jobs</li>
             <li>Browse</li>
           </ul>
-          <Popover >
-            <PopoverTrigger asChild>
-              <Avatar className="cursor-pointer">
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </PopoverTrigger>
-            <PopoverContent className='w-80'>
-                <div className="flex gap-2 space-y-2 align-center">
-                    
-              <Avatar className="cursor-pointer">
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <h4>Yashaswi MernStack</h4>
+          {!user ? (
+            <div className="flex items-center gap-2">
+              <Button variant="outline">Login</Button>
+              <Button className="bg-purple-400 hover:bg-purple-600">Signup</Button>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger asChild>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>PF</AvatarFallback>
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent className="w-80">
+                <div className="flex gap-2  items-center">
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>PF</AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium">Yashaswi MernStack</h4>
+                    <h4 className="text-sm text-mauve-400">
+                      Lorem ipsum dolor sit amet.
+                    </h4>
+                  </div>
                 </div>
-            </PopoverContent>
-          </Popover>
+                <div className="my-2">
+                  <div className="flex items-center w-fit cursor-pointer">
+                    <User2 />
+                    <Button variant="link">View Profile</Button>
+                  </div>
+                  <div className="flex items-center cursor-pointer">
+                    <LogOut />
+                    <Button variant="link">Logout</Button>
+                  </div>
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
         </div>
       </div>
     </div>
